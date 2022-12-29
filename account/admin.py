55d9ef8db74dac7ba55402c7c9dc24248e22b079
XiaoChin <xiaochin02@gmail.com>
2022-12-29 16:21:45 +0800

    feat: add and register account models

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, EmployeeProfile, VendorProfile
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(EmployeeProfile)
+admin.site.register(VendorProfile)
