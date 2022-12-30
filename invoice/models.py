97711c2e629f1d23e7e330c778b22362d6420d98
XiaoChin <xiaochin02@gmail.com>
2022-12-30 12:10:09 +0800

    feat: create invoice and item model

--- invoice/models.py
+++ invoice/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from account.models import EmployeeProfile, VendorProfile
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+
+
+class Invoice(models.Model):
+    invoice_id = models.CharField(max_length=20, primary_key=True)
+    date = models.DateField()
+    due_date = models.DateField()
+    vendor = models.ForeignKey(VendorProfile, on_delete=models.DO_NOTHING)
+    item = models.ManyToManyField(Item)
+    amount_charged = models.DecimalField(max_digits=10, decimal_places=2)
+    tax = models.IntegerField(blank=True)
+    amount_owned = models.DecimalField(max_digits=10, decimal_places=2)
+    uploader = models.ForeignKey(EmployeeProfile, on_delete=models.DO_NOTHING)
+    first_CFO_Approved = models.BooleanField(default=False)
+    second_CFO_Approved = models.BooleanField(default=False)
+
+    @property
+    def get_uploader_name(self):
+        return (self.uploader.first_name + " " + self.uploader.last_name)
+
+    @property
+    def get_vendor_name(self):
+        return self.vendor.name
+
+    @property
+    def get_item_list(self):
+        return (self.item.all())
+
+    def __str__(self):
+        return self.invoice_id
