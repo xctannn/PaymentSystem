75555dcbf072fd2312cf31fa6d803849e817f311
XiaoChin <xiaochin02@gmail.com>
2022-12-31 18:22:37 +0800

    feat: create basic invoice home view

--- invoice/views.py
+++ invoice/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Invoice
 
-# Create your views here.
+def home(request):
+    return render(request, 'invoice/home.html')
+
+class InvoiceListView(ListView):
+    model = Invoice
+    template_name = 'invoice/home.html'
+    context_object_name = 'invoices'
+    ordering = ['-date']  #order by date
+
+class InvoiceDetailView(DetailView):
+    model = Invoice
