16a3683e8a6f0a5c8e4fd3c0847e98531bdc60c9
XiaoChin <xiaochin02@gmail.com>
2023-01-04 02:23:44 +0800

    faet: add invoice-create path

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import InvoiceListView, InvoiceDetailView
+from .views import InvoiceListView, InvoiceDetailView, InvoiceCreateView
 
 urlpatterns = [
     path('', InvoiceListView.as_view(), name = 'invoice-home'),
+    path('new/', InvoiceCreateView.as_view(), name = 'invoice-create'),
     path('<pk>/', InvoiceDetailView.as_view(), name = 'invoice-detail'),
 ]
