8936afa2f8ac37743efb4b0a616cb932ef294334
XiaoChin <xiaochin02@gmail.com>
2023-01-04 02:23:05 +0800

    feat: change invoice and item relationship

--- invoice/models.py
+++ invoice/models.py
@@ -2,22 +2,11 @@ from django.db import models
 from account.models import EmployeeProfile, VendorProfile
 import datetime
 
-class Item(models.Model):
-    name = models.CharField(max_length=50)
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    
-    def __str__(self):
-        return self.name
-
-
 class Invoice(models.Model):
     invoice_id = models.CharField(max_length=20, primary_key=True)
     date = models.DateField()
     due_date = models.DateField()
     vendor = models.ForeignKey(VendorProfile, on_delete=models.DO_NOTHING)
-    item = models.ManyToManyField(Item)
     amount_charged = models.DecimalField(max_digits=10, decimal_places=2)
     tax = models.IntegerField(null=True)
     amount_owned = models.DecimalField(max_digits=10, decimal_places=2)
@@ -25,23 +14,16 @@ class Invoice(models.Model):
     department = models.CharField(max_length=50, choices=EmployeeProfile.Department.choices, blank=True)
     first_CFO_approved = models.BooleanField(default=False)
     second_CFO_approved = models.BooleanField(default=False)
-    approved_date = models.DateField(null=True)
+    approved_date = models.DateField(null=True, blank=True)
 
-    @property
     def get_uploader_name(self):
         return (self.uploader.first_name + " " + self.uploader.last_name)
 
-    @property
     def get_vendor_name(self):
         return self.vendor.name
 
-    @property
     def get_vendor_address(self):
         return self.vendor.address
-
-    @property
-    def get_item_list(self):
-        return (self.item.all())
 
     def set_department(self):
         self.department = self.uploader.department
@@ -63,5 +45,23 @@ class Invoice(models.Model):
         CFO = EmployeeProfile.objects.filter(position='CFO').last()
         return CFO.first_name + " " + CFO.last_name
 
+    def get_item_list(self):
+        return Item.objects.filter(invoice = self.invoice_id)
+
     def __str__(self):
         return self.invoice_id
+
+
+class Item(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+
+    def get_invoice_id(self):
+        return self.invoice.invoice_id
+    
