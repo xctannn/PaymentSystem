5754f8d59f12a2bc8b05b55eefe77b6e624069d6
XiaoChin <xiaochin02@gmail.com>
2023-01-05 16:11:46 +0800

    feat: add receipt into installed apps

--- PaymentSystem/settings.py
+++ PaymentSystem/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'invoice.apps.InvoiceConfig',
+    'receipt.apps.AccountConfig',
     'account.apps.AccountConfig',
     'crispy_forms',
     "bootstrap5",
