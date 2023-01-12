01d23f8d5a36c15fce4f7e955153ab10664e2a54
XiaoChin <xiaochin02@gmail.com>
2023-01-12 23:10:57 +0800

    feat: get request.user& add login required to view

--- payment/views.py
+++ payment/views.py
@@ -2,34 +2,50 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Payment
 from django.views.generic import ListView, DetailView, TemplateView
 from .forms import UploadPaymentForm 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from account.models import EmployeeProfile
 
-class PaymentListView(ListView):
+def is_CFO(User):
+    return User.groups.filter(name='CFO').exists()
+
+class PaymentListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Payment
     template_name = 'payment/home.html'
     context_object_name =  'payments'
-    ordering = ['payment_id']  #order by id
+    ordering = ['-payment_id']  #order by id
+    extra_context={"CFO": "CFO"}
+
+    def test_func(self):
+        return is_CFO(self.request.user)
 
 
-class PaymentDetailView(DetailView):
+class PaymentDetailView(LoginRequiredMixin, DetailView):
     model = Payment
+    extra_context={"CFO": "CFO"}
 
 
-class PaymentCreateView(TemplateView):    
+class PaymentCreateView(LoginRequiredMixin, TemplateView):    
     template_name =  'payment/payment_create_form.html'
     
     def get(self, request):
         context = {
             'UploadPaymentForm': UploadPaymentForm(),
+            'CFO' : "CFO",
         }
+        
         return render(request, self.template_name, context)
     
     def post(self, request):
         upload_payment_form = UploadPaymentForm(request.POST)
         if upload_payment_form.is_valid():
             upload_payment_form.save()
+            added_payment = Payment.objects.last()
+            employee = EmployeeProfile.objects.get(user = self.request.user)
+            added_payment.set_uploader(employee)
             return redirect('payment-home')
 
-
+@login_required
 def UpdatePayment(request, pk):
     object = get_object_or_404 (Payment, pk=pk)
     form = UploadPaymentForm(instance=object)
@@ -43,5 +59,6 @@ def UpdatePayment(request, pk):
     context = {
         "form": form,
         "object": object,
+        'CFO' : "CFO",
     }
     return render(request,  'payment/payment_update_form.html', context)
--- receipt/views.py
+++ receipt/views.py
@@ -2,31 +2,64 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Receipt
 from django.views.generic import ListView, DetailView, TemplateView
 from .forms import UploadReceiptForm
+from account.models import EmployeeProfile
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ReceiptListView(ListView):
+def is_CFO(User):
+    return User.groups.filter(name='CFO').exists()
+
+def is_FO(User):
+    return User.groups.filter(name='Finance Officer').exists()
+
+class ReceiptListView(LoginRequiredMixin, ListView):
     model = Receipt
     template_name = 'receipt/home.html'
     context_object_name = 'receipts'
     ordering = ['-receipt_id']  #order by id
 
+    def get_queryset(self):
+        self.employee = get_object_or_404(EmployeeProfile, user=self.request.user)
+        if is_CFO(self.request.user):
+            return Receipt.objects.all()
+        elif is_FO(self.request.user):
+            return Receipt.objects.filter(department=self.employee.department)
+    
+    def get_context_data(self, **kwargs):
+        context = super(ReceiptListView, self).get_context_data(**kwargs)
+        if is_CFO(self.request.user):
+            context['CFO'] = "CFO"
+        return context
 
-class RecieptDetailView(DetailView):
+
+class RecieptDetailView(LoginRequiredMixin, DetailView):
     model = Receipt
+    def get_context_data(self, **kwargs):
+        context = super(RecieptDetailView, self).get_context_data(**kwargs)
+        if is_CFO(self.request.user):
+            context['CFO'] = "CFO"
+        return context
 
 
-class ReceiptCreateView(TemplateView):    
+class ReceiptCreateView(LoginRequiredMixin, TemplateView):    
     template_name = 'receipt/receipt_create_form.html'
     
     def get(self, request):
         context = {
             'UploadReceiptForm': UploadReceiptForm(),
         }
+        if is_CFO(self.request.user):
+            context['CFO'] = "CFO"
         return render(request, self.template_name, context)
     
     def post(self, request):
         upload_receipt_form = UploadReceiptForm(request.POST)
         if upload_receipt_form.is_valid():
             upload_receipt_form.save()
+            added_receipt = Receipt.objects.last()
+            employee = EmployeeProfile.objects.get(user = self.request.user)
+            added_receipt.set_uploader(employee)
+            added_receipt.set_department()
             return redirect('receipt-home')
 
 
@@ -44,4 +77,6 @@ def UpdateReceipt(request, pk):
         "form": form,
         "object": object,
     }
+    if is_CFO(request.user):
+        context['CFO'] = "CFO"
     return render(request, 'receipt/receipt_update_form.html', context)
