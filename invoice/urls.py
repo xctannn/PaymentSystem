bf58eea9f77433ac157fbfeca8857fa5cbc0d699
XiaoChin <xiaochin02@gmail.com>
2023-01-12 21:48:06 +0800

    feat: add approve payment urls

--- invoice/urls.py
+++ invoice/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', InvoiceListView.as_view(), name = 'invoice-home'),
     path('new/', InvoiceCreateView.as_view(), name = 'invoice-create'),
     path('<pk>/', InvoiceDetailView.as_view(), name = 'invoice-detail'),
+    path('<pk>/approve', views.ApprovePayment, name = 'invoice-approve-payment'),
     path('<pk>/edit/', views.UpdateInvoice, name='invoice-edit'),
     path('<pk>/edit/item/', views.UpdateItem, name='item-edit'),
 ]
