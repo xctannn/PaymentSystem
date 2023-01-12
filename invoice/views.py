3abb58fe7baadfaa49f4f8d052a9674c7423c414
XiaoChin <xiaochin02@gmail.com>
2023-01-12 21:47:48 +0800

    feat: add user to views and login required

--- invoice/views.py
+++ invoice/views.py
@@ -1,43 +1,93 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import Invoice, Item
-from .forms import UploadInvoiceForm, AddItemForm, UpdateInvoiceForm
+from .forms import FOUploadInvoiceForm, CFOUploadInvoiceForm, AddItemForm, UpdateInvoiceForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from account.models import EmployeeProfile
 
-class InvoiceListView(ListView):
-    model = Invoice
+def is_CFO(User):
+    return User.groups.filter(name='CFO').exists()
+
+def is_FO(User):
+    return User.groups.filter(name='Finance Officer').exists()
+    
+class InvoiceListView(LoginRequiredMixin, ListView):
     template_name = 'invoice/home.html'
     context_object_name = 'invoices'
     ordering = ['-date']  #order by date
 
+    def get_queryset(self):
+        self.employee = get_object_or_404(EmployeeProfile, user=self.request.user)
+        if is_CFO(self.request.user):
+            return Invoice.objects.all()
+        elif is_FO(self.request.user):
+            return Invoice.objects.filter(department=self.employee.department)
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(InvoiceListView, self).get_context_data(**kwargs)
+        if is_CFO(self.request.user):
+            context['CFO'] = "CFO"
+        return context
 
-class InvoiceDetailView(DetailView):
+
+class InvoiceDetailView(LoginRequiredMixin, DetailView):
     model = Invoice
+    
+    def get_context_data(self, **kwargs):
+        context = super(InvoiceDetailView, self).get_context_data(**kwargs)
+        if is_CFO(self.request.user):
+            self.CFO = get_object_or_404(EmployeeProfile, user=self.request.user)
+            if (self.CFO == EmployeeProfile.objects.filter(position='CFO').first()):
+                context['CFO1'] = "CFO1"
+            else:
+                context['CFO2'] = "CFO2"
+        return context
 
 
-class InvoiceCreateView(TemplateView):    
+class InvoiceCreateView(LoginRequiredMixin, TemplateView):    
     template_name = 'invoice/invoice_create_form.html'
 
     def get(self, request):
-        context = {
-            'UploadInvoiceForm': UploadInvoiceForm(),
-        }
+        context = {}
+        if is_CFO(self.request.user):
+            context['CFO'] = "CFO"
+            context['UploadInvoiceForm'] = CFOUploadInvoiceForm()
+        if is_FO(self.request.user):
+            context['UploadInvoiceForm'] = FOUploadInvoiceForm()
         return render(request, self.template_name, context)
     
 
     def post(self, request):
-        upload_invoice_form = UploadInvoiceForm(request.POST)
+        CFO_upload_invoice_form = CFOUploadInvoiceForm(request.POST)
+        FO_upload_invoice_form = FOUploadInvoiceForm(request.POST)
         add_item_form = AddItemForm(request.POST)
         
-        if upload_invoice_form.is_valid():
-            upload_invoice_form.save()
+        if CFO_upload_invoice_form.is_valid():
+            CFO_upload_invoice_form.save()
             added_invoice = Invoice.objects.last()
+            employee = EmployeeProfile.objects.get(user = self.request.user)
+            added_invoice.set_uploader(employee)
+            context['CFO'] = "CFO"
+            context = {
+                'AddItemForm': AddItemForm(),
+                'added_invoice': added_invoice,
+            }
+            return render(request, self.template_name, context)
+
+        if FO_upload_invoice_form.is_valid():
+            FO_upload_invoice_form.save()
+            added_invoice = Invoice.objects.last()
+            employee = EmployeeProfile.objects.get(user = self.request.user)
+            added_invoice.set_uploader(employee)
             added_invoice.set_department()
             context = {
                 'AddItemForm': AddItemForm(),
                 'added_invoice': added_invoice,
             }
             return render(request, self.template_name, context)
-            
+
         if request.method =='POST' and 'additem' in request.POST and add_item_form.is_valid():
             name = add_item_form.cleaned_data['name']
             unit_price = add_item_form.cleaned_data['unit_price']
@@ -46,10 +96,18 @@ class InvoiceCreateView(TemplateView):
             added_invoice = Invoice.objects.last()
             item = Item.objects.create(invoice = added_invoice, name = name, unit_price = unit_price, quantity = quantity, total_price = total_price)
             item.save()
+            if is_CFO(self.request.user):
+                context['CFO'] = "CFO"
             context = {
                 'AddItemForm': AddItemForm(),
                 'UploadButton': 'UploadButton',
                 'added_invoice': added_invoice,
+            }
+            return render(request, self.template_name, context)
+
+        else:
+            context = {
+                'msg': "Invoice ID duplicate.",
             }
             return render(request, self.template_name, context)
 
@@ -58,7 +116,7 @@ def getCount(object):
     global x
     x = (object.get_item_list()).count()
 
-
+@login_required
 def UpdateInvoice(request, pk):
     object = get_object_or_404(Invoice, pk=pk)
     form = UpdateInvoiceForm(instance=object)
@@ -74,9 +132,11 @@ def UpdateInvoice(request, pk):
         "form": form,
         "object": object,
     }
+    if is_CFO(request.user):
+        context['CFO'] = "CFO"
     return render(request, 'invoice/invoice_update_form.html', context)
 
-
+@login_required
 def UpdateItem(request, pk):
     object = get_object_or_404(Invoice, pk=pk)
     global x
@@ -97,6 +157,8 @@ def UpdateItem(request, pk):
             "form": form,
             "object": object,
         }
+        if is_CFO(request.user):
+            context['CFO'] = "CFO"
         if x <= 0:
             return redirect ('invoice-detail', pk=pk)
         return render(request, 'invoice/item_update_form.html', context)
@@ -113,4 +175,17 @@ def UpdateItem(request, pk):
             "form": form,
             "object": object,
         }
+        if is_CFO(request.user):
+            context['CFO'] = "CFO"
         return render(request, 'invoice/item_update_form.html', context)
+
+
+@login_required
+def ApprovePayment(request, pk):
+    invoice = Invoice.objects.get(pk=pk)
+    if invoice.first_CFO_approved == False:
+        invoice.set_first_CFO_approve()
+    else:
+        invoice.set_second_CFO_approve()
+        invoice.set_approved_date()
+    return redirect('invoice-detail', pk=pk)
