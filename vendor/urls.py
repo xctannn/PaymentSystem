e71903bfa80f3b7b78be42a1da2cc5e34b5783d7
XiaoChin <xiaochin02@gmail.com>
2023-01-14 23:21:09 +0800

    feat: add all neecessary funtions to views

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import PaymentListView, ReceiptListView
+from .views import VendorReceiptListView, VendorPaymentListView, VendorPaymentDetailView, VendorReceiptDetailView
 from . import views
-from payment.views import VerifyPayment, DenyPayment, PaymentDetailView
-from receipt.views import RecieptDetailView
 
 urlpatterns = [
-    path('', PaymentListView.as_view(), name = 'vendor-home'),
-    path('receipt/', ReceiptListView.as_view(), name = 'vendor-receipt'),
-    path('<pk>/verify', VerifyPayment, name = 'payment-verify'),
-    path('<pk>/deny', DenyPayment, name = 'payment-deny'),
+    path('payment/', VendorPaymentListView.as_view(), name = 'vendor-payment-home'),
+    path('receipt/', VendorReceiptListView.as_view(), name = 'vendor-receipt-home'),
+    path('payment/<pk>', VendorPaymentDetailView.as_view(), name = 'vendor-payment-detail'),
+    path('receipt/<pk>', VendorReceiptDetailView.as_view(), name = 'vendor-receipt-detail'),
+    path('payment/<pk>/verify', views.VerifyPayment, name = 'vendor-payment-verify'),
+    path('payment/<pk>/deny', views.DenyPayment, name = 'vendor-payment-deny'),
 ]
