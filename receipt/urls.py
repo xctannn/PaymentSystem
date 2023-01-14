fb55f38dad6f0bd5141b430a6eca7bd3f2587d99
XiaoChin <xiaochin02@gmail.com>
2023-01-14 23:20:27 +0800

    refactor: correct the name ReceiptDetailView

--- receipt/urls.py
+++ receipt/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ReceiptListView, RecieptDetailView, ReceiptCreateView
+from .views import ReceiptListView, ReceiptDetailView, ReceiptCreateView
 from . import views
 
 urlpatterns = [
     path('', ReceiptListView.as_view(), name = 'receipt-home'),
     path('new/', ReceiptCreateView.as_view(), name = 'receipt-create'),
-    path('<pk>/', RecieptDetailView.as_view(), name = 'receipt-detail'),
+    path('<pk>/', ReceiptDetailView.as_view(), name = 'receipt-detail'),
     path('<pk>/edit/', views.UpdateReceipt, name='receipt-edit'),
 ]
