f2d68508f85bc3681af4bf655be1578b20c9d34c
XiaoChin <xiaochin02@gmail.com>
2023-01-15 20:23:36 +0800

    refactor: remove item display during edit

--- receipt/urls.py
+++ receipt/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ReceiptListView, ReceiptDetailView, ReceiptCreateView, ReceiptEditListView, RecieptEditDetailView
+from .views import ReceiptListView, ReceiptDetailView, ReceiptCreateView, ReceiptEditListView, ReceiptEditDetailView
 from . import views
 
 urlpatterns = [
@@ -9,7 +9,7 @@ urlpatterns = [
     path('<pk>/', ReceiptDetailView.as_view(), name = 'receipt-detail'),
     path('<pk>/edit/', views.UpdateReceipt, name='receipt-edit'),
     path('<pk>/editRequest/', views.RequestReceiptEdit, name='receipt-edit-request'),
-    path('<pk>/requestDetail/',RecieptEditDetailView.as_view(), name='receipt-edit-detail'),
+    path('<pk>/requestDetail/',ReceiptEditDetailView.as_view(), name='receipt-edit-detail'),
     path('<pk>/requestDetail/approve', views.ApproveReceiptRequestEdit, name='receipt-approve-edit'),
     path('<pk>/requestDetail/deny', views.DenyReceiptRequestEdit, name='receipt-deny-edit'),
 ]
